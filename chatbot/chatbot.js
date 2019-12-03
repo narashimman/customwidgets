4f64bfa082905a7f121c6953ac4ebf591bb76d82
narashimman <34731067+narashimman@users.noreply.github.com>
2019-12-03 14:17:56 +0530

    Update chatbot.js

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,9 +1,6 @@
 (function() {
     let template = document.createElement("template");
-    template.innerHTML = `
-  
-    <H1>HELLO WORLD 2</H1>
-    <style>
+    template.innerHTML = `<style>
     :host {
     border-radius: 25px;
     border-width: 4px;
@@ -11,14 +8,12 @@
     border-style: solid;
     display: block;
     }</style>
-   
+   <H1>HELLO WORLD 2</H1>
     <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
     channelId="f84fc5a3-1edf-4886-86dc-243d43fc1991"
     token="9351f7b04aab9b044f3960827697cabc"
     id="cai-webchat"
-    ></script>
-
-    `;
+    ></script>`;
     
     class chatbot extends HTMLElement {
     constructor() {
