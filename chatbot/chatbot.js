f5075fbc22e5e562fd9b690f4f98c96044858bc3
narashimman <34731067+narashimman@users.noreply.github.com>
2019-12-03 14:20:35 +0530

    Update chatbot.js

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,6 +1,12 @@
 (function() {
     let template = document.createElement("template");
     template.innerHTML = `
+    <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
+    channelId="f84fc5a3-1edf-4886-86dc-243d43fc1991"
+    token="9351f7b04aab9b044f3960827697cabc"
+    id="cai-webchat"
+    ></script>
+
     <H1>HELLO WORLD 2</H1>
     <style>
     :host {
