4b6459deb552e37e4189a5a246968181cf2c153e
narashimman <34731067+narashimman@users.noreply.github.com>
2019-12-09 04:59:49 +0400

    Add files via upload

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -1,12 +1,6 @@
 (function () {
     let template = document.createElement("template");
-    template.innerHTML = `
-    <H1> Hello World 3</H1>
-    HELLO WITH A CHAT BOT
-    <div class="container">
-    <h1 class="loader__title">0</h1>
-    <div class="loader"></div>   
-    </div>`;
+    template.innerHTML = `<H1> Hello World 3</H1>`;
 
     class helloworld extends HTMLElement {
         constructor() {
@@ -14,33 +8,6 @@
             let shadowRoot = this.attachShadow({ mode: "open" });
             shadowRoot.appendChild(template.content.cloneNode(true));
             console.log("Hello world!");
-        
-                const changeNum = () => {
-                const randomNum = Math.round(Math.random() * 100);
-                const degrees = Math.round((randomNum / 100) * 180);
-                const root = document.querySelector(":root");
-                let title = document.querySelector(".loader__title");
-
-                let currentNumber = title.innerText;
-
-                setInterval(() => {
-                    if (currentNumber < randomNum) {
-                        currentNumber++;
-                        title.innerText = currentNumber;
-                    } else if (currentNumber > randomNum) {
-                        currentNumber--;
-                        title.innerText = currentNumber;
-                    }
-                }, 3);
-
-                root.style.setProperty("--rotation", `${degrees}deg`);
-            };
-
-
-            setInterval(() => {
-                changeNum();
-            }, 2000);
-
             this.addEventListener("click", event => {
                 var event = new Event("onClick");
                 this.dispatchEvent(event);
